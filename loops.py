f3a3531a7246bb27ac067926cba969ee16949e1b
Aiden-L2 <Aidenrexl@nycstudents.net>
2026-09-15 09:21:16 -0400

    9/15/26

---
+++ loops.py
@@ -0,0 +1,18 @@
+import turtle
+from turtle import *
+t = Turtle()
+t.speed(200)
+
+for i in range(100):
+    t.forward(200)
+    t.left(90)
+    t.forward(200)
+    t.left(90)
+    t.forward(200)
+    t.left(90)
+    t.forward(200)
+    t.left(5)
+
+
+
+turtle.done()
