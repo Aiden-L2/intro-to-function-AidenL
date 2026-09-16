ae2a627e8e6b5994d04e3ec76558b93a6bc84508
Aiden-L2 <Aidenrexl@nycstudents.net>
2026-09-16 09:20:13 -0400

    9/16/26

--- app.py
+++ app.py
@@ -4,24 +4,37 @@ t = Turtle()
 
 t.shape('turtle')
 
+""" def triangle (x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+triangle(200) """
+
+""" def square (x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+square(200) """
+
+
 """ t.forward(200)
-t.left(120)
-t.forward(200)
-t.left(120)
-t.forward(200)
- """
-""" t.forward(200)
-t.left(90)
-t.forward(200)
-t.left(90)
-t.forward(200)
-t.left(90)
-t.forward(200)
- """
-t.forward(200)
 t.left(90)
 t.forward(200)
 t.left(135)
-t.forward(284)
+t.forward(284) """
 
 turtle.done()
+
+
+""" #define the function
+def add(x,y):
+    #variables here only accessible in the function, SCOPE
+    return x + y
+    #call to function
+z = add(5,15) """
--- loops.py
+++ loops.py
@@ -1,18 +1,31 @@
 import turtle
 from turtle import *
 t = Turtle()
-t.speed(200)
+t.speed(1000)
 
 for i in range(100):
-    t.forward(200)
-    t.left(90)
-    t.forward(200)
-    t.left(90)
-    t.forward(200)
-    t.left(90)
-    t.forward(200)
+    def square(x):
+        t.forward(x)
+        t.left(90)
+        t.forward(x)
+        t.left(90)
+        t.forward(x)
+        t.left(90)
+        t.forward(x)
+        t.left(90)
+    square(200)
     t.left(5)
 
+
+""" for i in range(100):
+    def triangle(x):
+        t.forward(x)
+        t.left(120)
+        t.forward(x)
+        t.left(120)
+        t.forward(x)
+        t.left(5)
+    triangle(200) """
 
 
 turtle.done()
