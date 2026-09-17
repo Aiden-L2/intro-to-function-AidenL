62839c2f62e9c019ccd82b748221884ee2d9b1dc
Aiden-L2 <Aidenrexl@nycstudents.net>
2026-09-17 09:21:09 -0400

    Update loops.py

--- loops.py
+++ loops.py
@@ -3,20 +3,53 @@ from turtle import *
 t = Turtle()
 t.speed(1000)
 
-for i in range(100):
-    def square(x):
+
+""" def square(x,y):
+    for i in range(4):
         t.forward(x)
-        t.left(90)
+        t.left(y)
+square(7,90)
+
+def doubleSquares(iRange):
+    length = 5
+    for i in range(iRange):
+        square(length, 90)
+        length = length * 1.07
+        t.right(5)
+doubleSquares(60) """
+
+def triangle(x,y):
+    for i in range(4):
         t.forward(x)
-        t.left(90)
-        t.forward(x)
-        t.left(90)
-        t.forward(x)
-        t.left(90)
-    square(200)
-    t.left(5)
+        t.left(y)
+triangle(7,120)
+
+def doubleTriangle(iRange):
+    length = 5
+    for i in range(iRange):
+        triangle(length, 120)
+        length = length * 1.07
+        t.left(75)
+doubleTriangle(60)
 
 
+
+
+""" for i in range(100):
+    def square(x,y):
+            t.forward(x)
+            t.left(y)
+            t.forward(x)
+            t.left(y)
+            t.forward(x)
+            t.left(y)
+            t.forward(x)
+            t.left(y)
+            t.left(5)
+    square(200,90)
+
+
+ """
 """ for i in range(100):
     def triangle(x):
         t.forward(x)
